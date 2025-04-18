39205ae3243dda898e1cb0e23faa8258fe1993df
Vladislove <vladislove05@mail.ru.com>
2025-04-18 10:51:11 +0500

    version1.0

---
+++ server.py
@@ -0,0 +1,56 @@
+import os
+from datetime import datetime
+from typing import Awaitable, Callable
+
+import aiohttp_jinja2
+import jinja2
+from aiohttp import web
+from aiohttp.web_app import Application
+from aiohttp.web_request import Request
+from aiohttp.web_response import StreamResponse
+
+from views import index
+from visit_counter import VisitCounter
+
+counter = VisitCounter()
+
+
+@web.middleware
+async def add_current_time(request: Request, handler: Callable[[Request], Awaitable[StreamResponse]]) -> StreamResponse:
+    request['current_time'] = datetime.now()
+    return await handler(request)
+
+
+async def handle(request: Request) -> StreamResponse:
+    client_ip = request.remote
+    current_time = request["current_time"]
+    counter.update(current_time, client_ip)
+
+    stats = {
+        "total_visits": counter.get_total_count(),
+        "unique_total_visits": counter.get_unique_total_count(),
+        "today_visits": counter.get_day_count(current_time),
+        "unique_today": counter.get_unique_day_count(current_time),
+        "month_visits": counter.get_month_count(current_time),
+        "unique_month": counter.get_unique_month_count(current_time),
+        "year_visits": counter.get_year_count(current_time),
+        "unique_year": counter.get_unique_year_count(current_time),
+    }
+    return web.json_response(stats)
+
+
+def setup_jinja(app: Application) -> None:
+    """
+    настройка Jinja2
+    """
+    template_dir = os.path.join(os.path.dirname(__file__), 'templates')
+    aiohttp_jinja2.setup(app, loader=jinja2.FileSystemLoader(template_dir))
+
+
+app = web.Application(middlewares=[add_current_time])
+setup_jinja(app)
+app['counter'] = counter
+app.router.add_get('/', index)
+
+if __name__ == '__main__':
+    web.run_app(app)
---
+++ tests/TestVisitTracker.py
@@ -0,0 +1,115 @@
+import os
+from datetime import datetime
+from typing import Any, Dict, Generator
+
+import pytest
+
+from visit_counter import VisitCounter
+
+
+@pytest.fixture
+def visit_counter(test_user_info: Dict[str, Any]) -> Generator[VisitCounter, None, None]:
+    counter = VisitCounter()
+    counter.filename = "test.json"
+    counter.data = test_user_info
+    yield counter
+    if os.path.exists("test.json"):
+        os.remove("test.json")
+
+
+@pytest.fixture()
+def test_user_info() -> Dict[str, Any]:
+    return {
+        "total_count": 30,
+        "day_count": {"2025-04-12": 5, "2024-03-15": 15},
+        "month_count": {"2025-04": 5, "2024-03": 15},
+        "year_count": {"2025": 5, "2024": 15},
+        "unique_ips": {
+            "total": {"176.30.122.233", "123.321.2.335", "190.123.23.22", "197.64.34.223", "121.34.555.212"},
+            "2025-04-12": {"176.30.122.233", "123.321.2.335"},
+            "2024-03": {"190.123.23.22", "197.64.34.223"},
+            "2024": {"190.123.23.22", "197.64.34.223"},
+        },
+    }
+
+
+def test_update(visit_counter: VisitCounter) -> None:
+    visit_counter.update(datetime(2025, 4, 12), "176.59.195.170")
+
+    assert visit_counter.data["total_count"] == 31
+    assert visit_counter.data["day_count"]["2025-04-12"] == 6
+    assert visit_counter.data["day_count"]["2024-03-15"] == 15
+    assert visit_counter.data["month_count"]["2025-04"] == 6
+    assert visit_counter.data["month_count"]["2024-03"] == 15
+    assert visit_counter.data["year_count"]["2025"] == 6
+    assert visit_counter.data["year_count"]["2024"] == 15
+
+    assert len(visit_counter.data["unique_ips"]["total"]) == 6
+    assert len(visit_counter.data["unique_ips"]["2025-04-12"]) == 3
+    assert len(visit_counter.data["unique_ips"]["2025-04"]) == 1
+    assert len(visit_counter.data["unique_ips"]["2024-03"]) == 2
+    assert len(visit_counter.data["unique_ips"]["2024"]) == 2
+
+
+def test_get_day_count(visit_counter: VisitCounter) -> None:
+    assert visit_counter.get_day_count(datetime(2025, 4, 12)) == 5
+
+    visit_counter.update(datetime(2025, 4, 12), "176.59.195.170")
+
+    assert visit_counter.get_day_count(datetime(2025, 4, 12)) == 6
+
+
+def test_get_month_count(visit_counter: VisitCounter) -> None:
+    assert visit_counter.get_month_count(datetime(2025, 4, 12)) == 5
+
+    visit_counter.update(datetime(2025, 4, 12), "176.59.195.170")
+
+    assert visit_counter.get_month_count(datetime(2025, 4, 12)) == 6
+
+
+def test_get_year_count(visit_counter: VisitCounter) -> None:
+    assert visit_counter.get_year_count(datetime(2025, 4, 12)) == 5
+
+    visit_counter.update(datetime(2025, 4, 12), "176.59.195.170")
+
+    assert visit_counter.get_year_count(datetime(2025, 4, 12)) == 6
+
+
+def test_get_total_count(visit_counter: VisitCounter) -> None:
+    assert visit_counter.get_total_count() == 30
+
+    visit_counter.update(datetime(2025, 4, 12), "176.59.195.170")
+
+    assert visit_counter.get_total_count() == 31
+
+
+def test_get_unique_day_count(visit_counter: VisitCounter) -> None:
+    assert visit_counter.get_unique_day_count(datetime(2025, 4, 12)) == 2
+
+    visit_counter.update(datetime(2025, 4, 12), "176.59.195.170")
+
+    assert visit_counter.get_unique_day_count(datetime(2025, 4, 12)) == 3
+
+
+def test_get_unique_month_count(visit_counter: VisitCounter) -> None:
+    assert visit_counter.get_unique_month_count(datetime(2025, 4, 12)) == 0
+
+    visit_counter.update(datetime(2025, 4, 12), "176.59.195.170")
+
+    assert visit_counter.get_unique_month_count(datetime(2025, 4, 12)) == 1
+
+
+def test_get_unique_year_count(visit_counter: VisitCounter) -> None:
+    assert visit_counter.get_unique_year_count(datetime(2024, 4, 12)) == 2
+
+    visit_counter.update(datetime(2024, 4, 12), "176.59.195.170")
+
+    assert visit_counter.get_unique_year_count(datetime(2024, 4, 12)) == 3
+
+
+def test_get_unique_total_count(visit_counter: VisitCounter) -> None:
+    assert visit_counter.get_unique_total_count() == 5
+
+    visit_counter.update(datetime(2024, 4, 12), "176.59.195.170")
+
+    assert visit_counter.get_unique_total_count() == 6
